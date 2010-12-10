615b90be8e5fe84534fbda77107ad04d4f63f94b
Jonathan Ballet <jonathan.ballet@securactive.net>
2010-12-10 16:30:28 +0100

    OK, actually, this is much simpler than I originally thought...
    I just run a small content script which add a div on top of the web page with the related links.
    
    The panel stuff was not really designed for this. I may have a look at the widget stuff instead.

--- data/script.js
+++ data/script.js
@@ -1,26 +1,32 @@
 // Link types supported (which allows navigation)
-var relKeywords = [    'start',
+var relKeywords = [
+    'start',
     'next',
     'prev',
     'contents',
     'index',
+    'top',
 ];
 
 
 $(document).ready(function() {
     var links = $('head link');
+    var nav_bar = $('<div></div>');
     var contents = [];
 
     links.each(function(i, link) {
         if ($.inArray(link.rel, relKeywords) != -1) {
-            contents.push({
-                'href': link.href,
-                'rel': link.rel,
-                'title': link.title});
+            var a = $('<a></a>');
+            a.attr({href: link.href, title: link.title});
+            a.text(link.rel);
+            nav_bar.append(a);
         }
     });
 
-    if (contents.length > 0) {
-        postMessage(contents);
+    if (nav_bar.children.length > 0) {
+        nav_bar.css('position', 'absolute')
+               .css('top', 0)
+               .css('background-color', 'red');
+        $('html').append(nav_bar);
     }
 });
