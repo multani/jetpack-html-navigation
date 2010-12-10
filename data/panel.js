1eab441f918d8761fed7b2416116187b27386bc7
Jonathan Ballet <jon@multani.info>
2010-12-10 16:30:28 +0100

    OK, actually, this is much simpler than I originally thought...
    I just run a small content script which add a div on top of the web page with the related links.
    
    The panel stuff was not really designed for this. I may have a look at the widget stuff instead.

--- data/panel.js
+++
@@ -1,9 +0,0 @@
-onMessage = function onMessage(links) {
-    var content = $('#content');
-
-    $.each(links, function(i, link) {
-        var a = '<a href="' + link['href'] + '" title="' + link['title'] + '">' + link['rel'] + '</a>';
-
-        content.append(a);
-    });
-}
